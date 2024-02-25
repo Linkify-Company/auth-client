6b1fb8f63bcdacda779d4ac6735a7db2e917eb88
VadimSavilov <savilov.vadim@ewaproduct.com>
2024-02-25 18:54:27 +0700

    implemented AuthFuncWithRoles

--- middleware.go
+++ middleware.go
@@ -2,6 +2,7 @@ package domain
 
 import (
 	"context"
+	"github.com/Linkify-Company/common_utils/errify"
 	"github.com/Linkify-Company/common_utils/logger"
 	"github.com/Linkify-Company/common_utils/response"
 	"net/http"
@@ -31,6 +32,29 @@ func (m *Middleware) AuthHandler(next http.Handler) http.Handler {
 		setCtx(r, authData)
 		next.ServeHTTP(w, r)
 	})
+}
+
+func (m *Middleware) AuthFuncWithRoles(roles ...Role) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			user, ok := GetAuthData(r)
+			if !ok {
+				response.Error(w,
+					errify.NewInternalServerError("could not get data from context",
+						"AuthFuncWithRoles/GetAuthData"), m.log)
+				return
+			}
+			for _, role := range roles {
+				if user.Role == role {
+					next.ServeHTTP(w, r)
+					return
+				}
+			}
+			response.Error(w,
+				errify.NewUnauthorizedError("not enough authority",
+					"User is not authorized", "AuthFuncWithRoles/GetAuthData"), m.log)
+		})
+	}
 }
 
 func GetAuthData(req *http.Request) (*AuthData, bool) {
